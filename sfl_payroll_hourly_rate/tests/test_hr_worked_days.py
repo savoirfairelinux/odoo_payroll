78783499db5c7dec8d7a5ae0c41e069ebf6b2ebe
David Dufresne <david.dufresne@savoirfairelinux.com>
2015-10-23 07:28:18 -0400

    Add hourly rates

---
+++ sfl_payroll_hourly_rate/tests/test_hr_worked_days.py
@@ -0,0 +1,69 @@
+# -*- coding:utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Savoir-faire Linux. All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.tests import common
+
+
+class TestHrWorkedDays(common.TransactionCase):
+    def setUp(self):
+        super(TestHrWorkedDays, self).setUp()
+        self.employee_model = self.env['hr.employee']
+        self.user_model = self.env["res.users"]
+        self.payslip_model = self.env["hr.payslip"]
+        self.worked_days_model = self.env["hr.payslip.worked_days"]
+        self.contract_model = self.env["hr.contract"]
+
+        self.employee = self.employee_model.create({
+            'name': 'Employee 1',
+        })
+
+        self.contract = self.contract_model.create({
+            'employee_id': self.employee.id,
+            'name': 'Contract 1',
+            'wage': 50000,
+        })
+
+        self.payslip = self.payslip_model.create({
+            'employee_id': self.employee.id,
+            'contract_id': self.contract.id,
+            'date_from': '2014-01-01',
+            'date_to': '2014-01-31',
+        })
+
+    def test_total(self):
+        worked_days = self.worked_days_model.create({
+            'date_from': '2014-01-01',
+            'date_to': '2014-01-05',
+            'number_of_hours': 40,
+            'hourly_rate': 25,
+            'rate': 150,
+            'payslip_id': self.payslip.id,
+            'code': 'test',
+            'name': 'test',
+            'contract_id': self.contract.id,
+        })
+
+        self.assertEqual(worked_days.total, 40 * 25 * 1.5)
+
+        worked_days.write({'rate': 200})
+        worked_days.refresh()
+
+        self.assertEqual(worked_days.total, 40 * 25 * 2.0)
