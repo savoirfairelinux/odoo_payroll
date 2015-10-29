397c691dd614f942832c090c38234e2e4e11da27
David Dufresne <david.dufresne@savoirfairelinux.com>
2015-10-28 23:08:05 -0400

    Add ytd amounts

---
+++ sfl_payroll_amount_ytd/README.rst
@@ -0,0 +1,24 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===========================
+Payslip Year-to-date Amount
+===========================
+
+Add the year-to-date amount on the payslip.
+The sum is based on the date of payment.
+
+Credits
+=======
+
+Contributors
+------------
+
+.. image:: http://sflx.ca/logo
+   :alt: Savoir-faire Linux
+   :target: http://sflx.ca
+
+* David Dufresne <david.dufresne@savoirfairelinux.com>
+* Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
+* Pierre Lamarche <pierre.lamarche@savoirfairelinux.com>
---
+++ sfl_payroll_amount_ytd/models/hr_payslip.py
@@ -0,0 +1,74 @@
+# -*- coding:utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Savoir-faire Linux
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from datetime import datetime
+
+from openerp import api, fields, models
+
+
+class HrPayslip(models.Model):
+
+    _inherit = 'hr.payslip'
+
+    @api.multi
+    def compute_sheet(self):
+        res = super(HrPayslip, self).compute_sheet()
+        self.compute_ytd_amounts()
+        return res
+
+    @api.one
+    def compute_ytd_amounts(self):
+        query = (
+            """SELECT pl_1.id, sum(
+                case when p.credit_note then -pl_2.amount else pl_2.amount end)
+            FROM hr_payslip_line pl_1, hr_payslip_line pl_2, hr_payslip p
+            WHERE pl_1.id IN %(payslip_line_ids)s
+            AND pl_1.salary_rule_id = pl_2.salary_rule_id
+            AND pl_2.slip_id = p.id
+            AND p.employee_id = %(employee_id)s
+            AND p.company_id = %(company_id)s
+            AND (p.state = 'done' OR p.id = %(payslip_id)s)
+            AND %(date_from)s <= p.date_payment
+            AND p.date_payment <= %(date_to)s
+            GROUP BY pl_1.id
+            """)
+
+        date_payment = fields.Date.from_string(self.date_payment)
+        date_from = fields.Date.to_string(
+            datetime(date_payment.year, 1, 1))
+
+        cr = self.env.cr
+
+        cr.execute(query, {
+            'date_from': date_from,
+            'date_to': self.date_payment,
+            'payslip_line_ids': tuple(self.line_ids.ids),
+            'employee_id': self.employee_id.id,
+            'company_id': self.company_id.id,
+            'payslip_id': self.id,
+        })
+
+        res = cr.fetchall()
+
+        line_model = self.env['hr.payslip.line']
+
+        for (line_id, amount_ytd) in res:
+            line = line_model.browse(line_id)
+            line.amount_ytd = amount_ytd
